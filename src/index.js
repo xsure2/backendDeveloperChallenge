f5227115dc35f2358811c8847eb3a803ee7c3938
xsure2 <83461208+xsure2@users.noreply.github.com>
2025-04-30 10:57:54 +0200

    primera aproximación a documentación swagger

--- src/index.js
+++ src/index.js
@@ -3,17 +3,38 @@ import dotenv from 'dotenv';
 import productRouter from  './products/infrastructure/ProductRouter.js';
 import testerRouter from './testers/infrastructure/TesterRouter.js';
 import testRouter from './tests/infrastructure/TestRouter.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.APP_HOST || 'hettp://localhost';
+
+
+const swaggerDocument = YAML.load('./documentation/base.yaml');
+
+const products = YAML.load('./documentation/products.yaml');
+const testers = YAML.load('./documentation/testers.yaml');
+const tests = YAML.load('./documentation/tests.yaml');
+
+swaggerDocument.paths = {
+  ...products.paths,
+  ...testers.paths,
+  ...tests.paths,
+};
+
 
 app.use(express.json());
 
+//Routes
 app.use('/testers', testerRouter);
 app.use('/products', productRouter);
 app.use('/tests', testRouter);
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -23,5 +44,6 @@ app.use((err, req, res, next) => {
 export default app;
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${host}:${port}`);
+  console.log(`Swagger docs available at ${host}:${port}/api-docs`);
 });
